a6f4589622740c7b84598059536d4cf6915e4111
Steve <ycsteven.tsao@gmail.com>
2020-05-29 06:52:33 +0800

    copy file to public

---
+++ public/precache-manifest.2e67e3a2d7a7948c5e4e583ed496fa4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec3fa1a33f3a6bb787d8c12e2cfe968",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf9eb2ad1b18cb0c67d8",
+    "url": "/static/css/main.465593ce.chunk.css"
+  },
+  {
+    "revision": "300b088584b036c08b0d",
+    "url": "/static/js/2.b7f5c19a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b7f5c19a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf9eb2ad1b18cb0c67d8",
+    "url": "/static/js/main.25ef4659.chunk.js"
+  },
+  {
+    "revision": "472e83ae06a395511031",
+    "url": "/static/js/runtime-main.05b1986e.js"
+  }
+]);
